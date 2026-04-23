54c23fb38864b2d01df79700ae34471adb2bcbf0
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-23 15:15:03 +0200

    create logout-button and using it in navbar

---
+++ src/components/buttons/LogoutButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { logoutAction } from "../../actions/auth.actions";
+
+
+type Props = {
+  title: string,
+  isMobile: boolean
+}
+
+const LogoutButton = ({ title, isMobile }: Props) => {
+
+ const handleLogout = async () => {
+    await logoutAction()
+    
+  }
+
+  return (
+    <button
+
+      onClick={handleLogout}
+      className={`
+        ${isMobile ? 'block w-full px-3 py-2 rounded-md text-base font-medium' : 'px-5 py-2 text-sm font-medium'}
+        text-red-600 hover:bg-red-50 rounded-lg transition-colors
+        dark:text-gray-100 dark:hover:text-red-500 dark:hover:bg-gray-100
+      `}
+    >
+      {title}
+    </button>
+  )
+}
+
+export default LogoutButton
