91d6ec50b3e25af6a67a043926d06373cca6386d
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-24 00:35:54 +0200

    add formatDate in table

--- src/components/data-tables/Table.tsx
+++ src/components/data-tables/Table.tsx
@@ -8,8 +8,13 @@ type Props<T = any> = {
     fields: string[]
     isActions: boolean
 }
-const Table = ({docs, fields, isActions }: Props) => {
-    const {role} = useAppSelector(state => state.auth)
+function Table({ docs, fields, isActions }: Props) {
+    const { role } = useAppSelector(state => state.auth)
+
+     const formatDate = (cellValue: any) => {
+    if(!cellValue) return cellValue
+   return new Date(cellValue!!).toISOString().slice(0,10)
+}
 
 
     return (
@@ -17,17 +22,17 @@ const Table = ({docs, fields, isActions }: Props) => {
             <div className="flex flex-col md:flex-row justify-between gap-4 pb-5 px-4 " >
                 <table className="min-w-full table-fixed divide-y divide-gray-200 dark:divide-gray-700 shadow-sm">
                     <colgroup>
-                        <col style={{ width: "5%" }} />   
-                        <col style={{ width: "50%", marginLeft: '50px' }} /> 
-                        <col style={{ width: "20%" }} />  
-                        <col style={{ width: "15%" }} />  
+                        <col style={{ width: "5%" }} />
+                        <col style={{ width: "50%", marginLeft: '50px' }} />
+                        <col style={{ width: "20%" }} />
+                        <col style={{ width: "15%" }} />
                     </colgroup>
 
                     <thead className="bg-gray-50 dark:bg-gray-800">
                         <tr>
-                        {fields && fields.map((f, index) => (
-                            <th key={index} className="px-3 py-2 text-center text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">{f}</th>
-                        ))}
+                            {fields && fields.map((f, index) => (
+                                <th key={index} className="px-3 py-2 text-center text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">{f}</th>
+                            ))}
                         </tr>
                     </thead>
 
@@ -36,38 +41,38 @@ const Table = ({docs, fields, isActions }: Props) => {
                             <tr key={index} className="text-sm text-gray-500 dark:text-gray-300 hover:bg-gray-300 hover:text-gray-700">
                                 <td className="px-3 py-2 whitespace-nowrap">{m.id}</td>
 
-                              {m.email &&  <td
+                                {m?.email && <td
                                     className="px-3 py-2 truncate overflow-hidden"
                                     title={m.email}
                                 >
                                     {m.email}
                                 </td>}
 
-                                 {m.action &&  <td
+                                {m.action && <td
                                     className="px-3 py-2 truncate overflow-hidden"
                                     title={m.action}
                                 >
                                     {m.action}
                                 </td>}
 
-                                {m.timestamp &&  <td
+                                {m.timestamp && <td
                                     className="px-3 py-2 truncate overflow-hidden"
                                     title={m.timestamp}
                                 >
-                                    {m.timestamp}
+                                    {formatDate(m.timestamp)}
                                 </td>}
 
-                                
 
-                               {m.role && <td className="px-3 py-2 whitespace-nowrap">{m.role}</td>}
+
+                                {m.role && <td className="px-3 py-2 whitespace-nowrap">{m.role}</td>}
 
                                 <td className="px-3 py-2">
-                                   {isActions && role === 'admin' && <div className="flex flex-col md:flex-row gap-2">
-                                         <Link to={`/profile/${m.id}/view`} 
-                                        className="cursor-pointer text-blue-700 border border-blue-700 hover:border-0 hover:bg-blue-500 hover:text-white px-3 py-1 rounded">View</Link>
-                                        <Link to={`/profile/${m.id}/edit`} 
-                                        className="cursor-pointer text-green-700 border border-green-700 hover:border-0 hover:bg-green-500 hover:text-white px-3 py-1 rounded">Edit</Link>
-                                    </div> }
+                                    {isActions && role === 'admin' && <div className="flex flex-col md:flex-row gap-2">
+                                        <Link to={`/profile/${m.id}/view`}
+                                            className="cursor-pointer text-blue-700 border border-blue-700 hover:border-0 hover:bg-blue-500 hover:text-white px-3 py-1 rounded">View</Link>
+                                        <Link to={`/profile/${m.id}/edit`}
+                                            className="cursor-pointer text-green-700 border border-green-700 hover:border-0 hover:bg-green-500 hover:text-white px-3 py-1 rounded">Edit</Link>
+                                    </div>}
                                 </td>
                             </tr>
                         ))}
