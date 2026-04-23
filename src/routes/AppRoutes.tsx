8e82e7b2d3e1ca80cb1b6caa728a9a65dd15f43b
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-23 15:15:51 +0200

    create app routes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode } from 'react'
+import { Routes   , Route, useLocation, Navigate } from 'react-router';
+import DashboardPage from '../pages/Dashboard'
+import UsersPage from '../pages/Users'
+import ProfilePage from '../pages/Profile'
+import RegisterPage from '../auth/Register';
+import LoginPage from '../auth/Login';
+import { useAppSelector } from '../store/store';
+import { getToken } from '../lib/token';
+import { getRole } from '../lib/role';
+
+const ProtectedRoute = ({children, roles}: {children: ReactNode, roles: string[]}) => {
+    const {user, role} = useAppSelector(state => state.auth)
+    const location = useLocation()
+    
+
+    if(!user){
+        return <Navigate to="/login" state={{ from: location}} replace />
+    }
+
+    if(roles && !roles.includes(role!!)){
+        return <Navigate to="/" replace />
+    }
+
+    return children
+
+}
+
+const AppRoutes = () => {
+    const {user, isAuth, token} = useAppSelector(state => state.auth)
+    //const token = getToken()
+    console.log("routes token:", isAuth, token)
+    console.log("routes user:", user)
+
+    return (
+        <Routes>
+            <Route index element={<ProtectedRoute roles={['admin', 'user']}><DashboardPage /></ProtectedRoute>} />
+            <Route path="/" element={<ProtectedRoute roles={['admin', 'user']}><DashboardPage /></ProtectedRoute>} />
+            <Route path="/users" element={<ProtectedRoute roles={['admin']}><UsersPage /></ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute roles={['admin', 'user']}><ProfilePage /></ProtectedRoute>} />
+            <Route path="/profile/:id/view" element={<ProtectedRoute roles={['admin']}><ProfilePage /></ProtectedRoute>} />
+            <Route path="/profile/:id/edit" element={<ProtectedRoute roles={['admin']}><ProfilePage /></ProtectedRoute>} />
+            <Route path="/login" element={isAuth || token ? <Navigate to="/" /> : <LoginPage />} />
+            <Route path="/register" element={isAuth || token ? <Navigate to="/" /> : <RegisterPage />} />
+        </Routes>
+    )
+}
+
+export default AppRoutes
+
