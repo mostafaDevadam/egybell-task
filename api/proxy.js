bbd25e51ef9b14af3f5364a3d5d80fe8f7b665f4
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-24 17:24:24 +0200

    add api/prox.js

---
+++ api/proxy.js
@@ -0,0 +1,14 @@
+// api/proxy.js
+import fetch from 'node-fetch';
+export default async (req, res) => {
+  const path = req.url.replace(/^\/api/, '');
+  const upstream = `${process.env.VITE_LIVE_API_URL}${path}`; // http://...
+  const response = await fetch(upstream, {
+    method: req.method,
+    headers: Object.fromEntries(Object.entries(req.headers).filter(([k])=>k!=='host')),
+    body: ['GET','HEAD'].includes(req.method) ? undefined : req
+  });
+  res.status(response.status);
+  response.headers.forEach((v,k)=>res.setHeader(k,v));
+  response.body.pipe(res);
+};
