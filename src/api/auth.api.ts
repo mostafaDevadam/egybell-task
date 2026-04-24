c5131cb8c3ca7ba4c04bc6b41795976e2ec4ba2b
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-24 18:06:14 +0200

    update auth api

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -13,6 +13,14 @@ export const loginAPI = async (body: AUTH_BODY_TYPE) => {
 
     console.log("resp:", resp)
 
+     const resp2 = await fetch(String(import.meta.env.VITE_LIVE_API_URL)+'/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ body })
+    });
+
+     console.log("resp2:", resp2)
+
 
     const response = await callAPI<AUTH_RESPONSE_TYPE, AUTH_BODY_TYPE>({ url: `${prefix}/login`, method: Method.POST, body: body })
     return response
