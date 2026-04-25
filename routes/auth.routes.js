f4f7ed9f01609a1e89da82ccf707cddd68d71f3e
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-25 20:42:46 +0100

    update expire token

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -130,7 +130,7 @@ function generateAccessToken(user) {
   const token = jwt.sign(
     { id: user.id, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.EXPRIES_IN }
+    { expiresIn: process.env.EXPRIES_IN ?? "5m"}
   );
   return token
 }
