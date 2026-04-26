52fc9513858538b5961a6e2f71aeeac42e39d97c
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-26 02:14:25 +0100

    handle dark mode for table

--- src/components/data-tables/Table.tsx
+++ src/components/data-tables/Table.tsx
@@ -49,7 +49,7 @@ function Table({ docs, fields, isActions }: Props) {
 
                         {docs && docs.map((m, index) => (
                             m.id !== user?.id &&
-                            <tr key={index} className="text-sm text-gray-500 dark:text-gray-300 hover:bg-gray-300 hover:text-gray-700">
+                            <tr key={index} className="text-sm text-gray-500 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:*:text-white hover:bg-gray-300 hover:text-gray-700">
                                 {
                                     fields && fields.map((f, index) => (
                                         <td key={index} className="px-3 py-2 whitespace-nowrap">
