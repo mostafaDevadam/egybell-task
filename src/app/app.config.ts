5609b1d18ee115468974c7bb1435936f8b3faed4
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-30 03:54:46 +0100

    update app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import {
   provideAppInitializer,
   provideBrowserGlobalErrorListeners,
   isDevMode,
+  InjectionToken,
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
@@ -17,8 +18,39 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { reducers } from './store/store';
 import { AuthEffects } from './store/auth.store';
 
+import { provideNgxSignalToast } from 'ngx-signal-toast';
+import { provideToastr } from 'ngx-toastr';
+
+
+
+
+
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
+
+
+    provideToastr(), 
+
+    provideNgxSignalToast({
+      position: 'top-left',      // where toasts appear
+      duration: 4000,             // auto-dismiss in ms (0 = never)
+      theme: 'material',  
+            
+      layout: 'card',        // layout style
+      animation: 'slide',          // enter/leave animation
+      maxToasts: 5,                // max visible at once
+      showProgress: true,             // show countdown bar
+      pauseOnHover: true,             // pause timer on hover
+      closeOnClick: false,            // dismiss on toast click
+      closable: true,             // show close button
+      dedupe: false,            // prevent duplicate messages
+      rtl: false,            // right-to-left mode
+      zIndex: 9999,             // CSS z-index of container
+    }),
+
+
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding())/*provideClientHydration(withEventReplay()),*/,
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
