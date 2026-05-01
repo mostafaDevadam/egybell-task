f5d0151dff371ecaeb545ee3abfb7b34891e0256
mostafaDevadam <adam.mostafa@outlook.de>
2026-05-01 11:29:49 +0100

    update redis service with new functions

--- services/redis.service.js
+++ services/redis.service.js
@@ -3,11 +3,28 @@ const { createClient } = require('redis');
 
 const redis = createClient();
 
+
+
 const connectRedis = async () => {
     await redis.connect();
     console.log('Redis connected');
 }
 
+const saveCount = async (key, count) => {
+    await redis.set(key, count);
+}
+
+const saveObject = async (key, object) => {
+    await redis.set(key, JSON.stringify(object));
+}
+
+const getObject = async (key) => {
+    return JSON.parse(await redis.get(key))
+}
+
+const getCount = async (key) => {
+    return await redis.get(key)
+}
 
 
-module.exports.RedisService = { connectRedis }
+module.exports.RedisService = { redis, connectRedis, saveCount, getCount, saveObject, getObject }
