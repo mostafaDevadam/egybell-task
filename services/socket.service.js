37fb6b6519d60f63df5ce8c78c94f9fcac5119af
mostafaDevadam <adam.mostafa@outlook.de>
2026-05-01 11:30:30 +0100

    update socket service

--- services/socket.service.js
+++ services/socket.service.js
@@ -1,6 +1,8 @@
 const Socket = require("socket.io");
 const { AuthService } = require("../routes/auth.routes");
 const { NotificationsService } = require("../services/notifications.service");
+const i18n = require("../i18n");
+const jwt = require("jsonwebtoken");
 
 let notifications_ = []
 let response_object = {
@@ -11,33 +13,60 @@ let response_object = {
 
 let io
 const connectSocket = (httpServer) => {
-    io = new Socket.Server(httpServer,{
+    io = new Socket.Server(httpServer, {
         cors: {
             //origin: "*",
-            
+
         }
     })
     io.use((socket, next) => {
-        const token = socket.handshake.query.token;
+        const token = socket.handshake.auth.token;
+        const lang = socket.handshake.auth.lang
 
-        console.log("token:", token, socket.handshake)
+        console.log("socket token:", token, lang)
+
+        socket.lang = lang ?? "ar";
+
+        socket.__ = (key, params) => {
+            return i18n.__({ phrase: key, locale: socket.lang }, params);
+        };
 
         /*if (!token) return next(new Error("No token"));
 
-        try {
+        const user = AuthService.verifyAccessToken(token);
+        if (user) {
+            socket.user = user;
+            next();
+        } else {
+            next(new Error("Unauthorized"));
+        }*/
+
+        if (!token) {
+            return next(new Error("Unauthorized"));
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) return next(new Error("Unauthorized"));
+            socket.user = decoded;
+            next();
+        });
+
+
+        /*try {
             const user = AuthService.verifyAccessToken(token);
             socket.user = user;
             next();
         } catch {
             next(new Error("Unauthorized"));
         }*/
-        next()
+
+        //next()
     })
     io.on("connection", (socket) => {
         console.log("User connected:", socket.id);
 
         // send message to client
-        socket.emit("message", "Hello from server!");
+        socket.emit("message", "Hello from server!" /*+ socket.__("main.api")*/);
 
         sendAllNotifications(notifications_ ?? NotificationsService.findAll())
 
@@ -61,7 +90,7 @@ const connectSocket = (httpServer) => {
                 io.emit("read-notification", "no notification found")
                 //throw new Error("no notification found")
             }
-            console.log("notify:", notify)
+            console.log("as read notify:", notify)
             io.emit("read-notification", notify);
             sendAllNotifications(notifications_)
         });
@@ -78,15 +107,16 @@ const sendMessge = (key, msg) => {
     io.emit(key, msg);
 }
 
-const sendNotification = (data) => {
+const sendNotification = (statusCode = 201, message = "send notification", data) => {
     response_object.data = data
-    response_object.statusCode = 201
-    response_object.message = "send notification"
+    response_object.statusCode = statusCode
+    response_object.message = message
+    console.log("notification:", data)
     io.emit("notification", response_object);
     io.on("notification", (data_) => {
         console.log("Notification:", data_);
     });
-     clearResponse()
+    clearResponse()
 }
 
 const sendAllNotifications = (notifications) => {
@@ -94,12 +124,13 @@ const sendAllNotifications = (notifications) => {
     response_object.data = notifications
     response_object.statusCode = 201
     response_object.message = "Get All notifications"
+    console.log("notifications:", notifications)
     io.emit("notifications", response_object);
     clearResponse()
 }
 
 const clearResponse = () => {
-      response_object.data = null
+    response_object.data = null
     response_object.statusCode = 0
     response_object.message = ""
 }
