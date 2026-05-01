d63e6e4baa5b87df04f0e7fbda012c86de1d971f
mostafaDevadam <adam.mostafa@outlook.de>
2026-05-01 04:39:15 +0100

    test wit console.log

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,13 +3,17 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
+    console.log("headers: ", req.headers)
+
   if (!token) return res.status(401).json({ message: "No token" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // { id, role }
+    console.log("success token user:", req.user, token)
     next();
   } catch (err) {
+    console.log("err:", err)
     res.status(401).json({ message: "Invalid token" });
   }
 };
