5b4145007d0fa87c3d833a2175f2c3fcadc2c0e6
mostafaDevadam <adam.mostafa@outlook.de>
2026-05-01 04:28:20 +0100

    update logout button

--- src/app/components/buttons/logout-button/logout-button.ts
+++ src/app/components/buttons/logout-button/logout-button.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../../auth/auth-service';
 import { Store } from '@ngrx/store';
 import { logoutAction } from '../../../store/auth.store';
 import { MessageService } from '../../toast-uis/message/message-service';
+import { AuthSignalStore } from '../../../signal-store/auth-signal.store';
 
 @Component({
   selector: 'app-logout-button',
@@ -15,11 +16,14 @@ export class LogoutButton {
   service = inject(AuthService)
   private store = inject(Store)
   messageService = inject(MessageService)
+    private authSignalStore = inject(AuthSignalStore);
+  
 
 
   logout() {
     this.service.logout()
     this.store.dispatch(logoutAction())
+    this.authSignalStore.logout()
     this.messageService.showStacked(
       'Logout successfully',
       4
